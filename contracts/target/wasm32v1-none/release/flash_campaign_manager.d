6a3f450500b198a187c5d03d8a7e3e8fb4f15127
Timothy Baker <35656692+silence48@users.noreply.github.com>
2025-05-15 17:51:45 -0400

    contracts build

---
+++ contracts/target/wasm32v1-none/release/flash_campaign_manager.d
@@ -0,0 +1,1 @@
+C:\flashtoken\github\flashpool-protocol\contracts\target\wasm32v1-none\release\flash_campaign_manager.wasm: C:\flashtoken\github\flashpool-protocol\contracts\flash_campaign_manager\src\lib.rs
