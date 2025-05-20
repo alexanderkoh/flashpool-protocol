12329175da0b8861e33f23303024ced2f9e72e5e
Timothy Baker <35656692+silence48@users.noreply.github.com>
2025-05-20 16:52:38 -0400

    update build

--- contracts/target/wasm32v1-none/release/deps/flash_campaign_manager.d
+++ contracts/target/wasm32v1-none/release/deps/flash_campaign_manager.d
@@ -1,5 +1,7 @@
-C:\flashtoken\github\flashpool-protocol\contracts\target\wasm32v1-none\release\deps\flash_campaign_manager.wasm: flash_campaign_manager\src\lib.rs
+C:\flashtoken\github\flashpool-protocol\contracts\target\wasm32v1-none\release\deps\flash_campaign_manager.wasm: flash_campaign_manager\src\lib.rs flash_campaign_manager\src\rewards.rs flash_campaign_manager\src\storage.rs
 
-C:\flashtoken\github\flashpool-protocol\contracts\target\wasm32v1-none\release\deps\flash_campaign_manager.d: flash_campaign_manager\src\lib.rs
+C:\flashtoken\github\flashpool-protocol\contracts\target\wasm32v1-none\release\deps\flash_campaign_manager.d: flash_campaign_manager\src\lib.rs flash_campaign_manager\src\rewards.rs flash_campaign_manager\src\storage.rs
 
 flash_campaign_manager\src\lib.rs:
+flash_campaign_manager\src\rewards.rs:
+flash_campaign_manager\src\storage.rs:
--- contracts/target/wasm32v1-none/release/flash_campaign_manager.d
+++ contracts/target/wasm32v1-none/release/flash_campaign_manager.d
@@ -1,1 +1,1 @@
-C:\flashtoken\github\flashpool-protocol\contracts\target\wasm32v1-none\release\flash_campaign_manager.wasm: C:\flashtoken\github\flashpool-protocol\contracts\flash_campaign_manager\src\lib.rs
+C:\flashtoken\github\flashpool-protocol\contracts\target\wasm32v1-none\release\flash_campaign_manager.wasm: C:\flashtoken\github\flashpool-protocol\contracts\flash_campaign_manager\src\lib.rs C:\flashtoken\github\flashpool-protocol\contracts\flash_campaign_manager\src\rewards.rs C:\flashtoken\github\flashpool-protocol\contracts\flash_campaign_manager\src\storage.rs
